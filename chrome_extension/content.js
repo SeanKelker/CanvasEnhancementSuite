78ce8bc2b2899b9b60d6cf03488d3ef4f7b303c5
seankelker <seankelker@gmail.com>
2020-11-27 20:12:27 -0800

    Added Course Headers

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,54 +1,46 @@
-//Add header to camino
-let main_window = document.body;
-let font = document.createElement('link');
-font.href = "https://use.typekit.net/fmp3diy.css";
-font.rel = "stylesheet";
-document.head.appendChild(font);
+function createHeader(courses) {
+    courses = courses
+        .filter(c => c.course_code)
+        .filter(c => new Date(c['end_at']) >= Date.now())
 
-function createHeader () {
     let header = document.createElement('ul');
     header.setAttribute('id', 'ces-header');
     let title = '<li class="bar-item" id="ces-header-title">CES</li>';
-
-    //CLASS 1
-    let class1 = document.createElement('li');
-    class1.className = 'bar-item ces-dropdown';
-
-    let dropbtn1 = document.createElement('a');
-    dropbtn1.className = "dropbtn";
-    dropbtn1.innerHTML = "COEN174";
-    dropbtn1.href = "javascript:void(0)";
-
-    let dropContent1 = document.createElement('div');
-    dropContent1.className = 'dropdown-content';
-
-    let contentStr1 = '<a href="#">Assignments</a><a href="#">Announcements</a>'
-    dropContent1.insertAdjacentHTML('beforeend', contentStr1);
-
-    class1.appendChild(dropbtn1);
-    class1.appendChild(dropContent1);
-
-    //CLASS 2
-    let class2 = document.createElement('li');
-    class2.className = 'bar-item ces-dropdown';
-
-    let dropbtn2 = document.createElement('a');
-    dropbtn2.className = "dropbtn";
-    dropbtn2.innerHTML = "COEN150";
-    dropbtn2.href = "javascript:void(0)";
-
-    let dropContent2 = document.createElement('div');
-    dropContent2.className = 'dropdown-content';
-
-    let contentStr2 = '<a href="#">Assignments</a><a href="#">Announcements</a>'
-    dropContent2.insertAdjacentHTML('beforeend', contentStr2);
-    class2.appendChild(dropbtn2);
-    class2.appendChild(dropContent2);
-
     header.insertAdjacentHTML('beforeend', title);
-    header.appendChild(class1);
-    header.appendChild(class2);
+    
+    for (let i = 0; i < courses.length; i++) {
+        const c = courses[i];
+
+        let course = document.createElement('li');
+        course.className = 'bar-item ces-dropdown';
+
+        let dropbtn = document.createElement('a');
+        dropbtn.className = "dropbtn";
+        dropbtn.innerHTML = c['course_code'];
+        dropbtn.href = "javascript:void(0)";
+
+        let dropContent = document.createElement('div');
+        dropContent.className = 'dropdown-content';
+
+        let contentStr = '<a href="#">Assignments</a><a href="#">Announcements</a>'
+        dropContent.insertAdjacentHTML('beforeend', contentStr);
+
+        course.appendChild(dropbtn);
+        course.appendChild(dropContent);
+        header.appendChild(course);
+    }
     return header;
 }
 
-main_window.prepend(createHeader());
+fetch('https://camino.instructure.com/api/v1/courses')
+    .then(result => result.text())
+    .then(result => {
+        courses = JSON.parse(result.substr(9, result.length));
+        console.log(courses)
+        let main_window = document.body;
+        let font = document.createElement('link');
+        font.href = "https://use.typekit.net/fmp3diy.css";
+        font.rel = "stylesheet";
+        document.head.appendChild(font);
+        main_window.prepend(createHeader(courses));
+    });
