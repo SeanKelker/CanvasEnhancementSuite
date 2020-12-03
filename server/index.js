4974d8e4e738a0e4953b465d19838bb93eee56a2
Zak Graber <zakgraber@gmail.com>
2020-12-03 13:29:34 -0800

    Fixing Sean's thing

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const {Connection} = require('./mongo.js');
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "*")
+    next();
+});
+
 app.get('/courses', async function(req, res){
     let userName = req.query.name;
     let userId = req.query.id;
