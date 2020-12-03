5b49956c30ade93fb7d0e1edacb825a10f8f7f41
seankelker <seankelker@gmail.com>
2020-12-03 12:40:46 -0800

    add site blocking

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,17 +1,78 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({
-        color: '#3aa757'
-    }, function () {
-        console.log('The color is green.');
+(function () {
+
+    var storage = chrome.storage;
+
+    function loadWebsites(callback) {
+        var websites;
+
+        storage.local.get(["defaultWebsites", "customWebsites"], function (items) {
+            if (items.defaultWebsites === undefined) {
+                websites =
+                    [
+                        {"url": "facebook.com", "on": true},
+                        {"url": "twitter.com", "on": true},
+                        {"url": "linkedin.com", "on": true},
+                        {"url": "instagram.com", "on": true},
+                        {"url": "youtube.com", "on": true},
+                    ];
+
+                storage.local.set({"defaultWebsites": websites});
+            } else {
+                websites = items.defaultWebsites;
+            }
+
+            if (items.customWebsites === undefined) {
+                storage.local.set({"customWebsites": []});
+            } else {
+                websites = websites.concat(items.customWebsites);
+            }
+
+            if (typeof callback === "function") {
+                callback(websites);
+            }
+        });
+    }
+
+    function urlContains(url, keywords) {
+        var result = false;
+
+        for (var index in keywords) {
+            if (keywords[index].on && url.indexOf(keywords[index].url) != -1) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    function analyzeUrl(details) {
+        storage.local.get("on", function (item) {
+            if (item.on === true) {
+
+                loadWebsites(function (websites) {
+                    if (details.frameId === 0 && urlContains(details.url, websites)) {
+                        var id = details.tabId;
+
+                        chrome.tabs.update(id, {"url": "https://camino.instructure.com/"});
+
+                        storage.local.get("blocked", function (item) {
+                            storage.local.set({"blocked": item.blocked + 1});
+                            console.log(item);
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    chrome.webNavigation.onCommitted.addListener(analyzeUrl);
+
+    storage.local.get("on", function (item) {
+        if (item.on === undefined) {
+            storage.local.set({"on": false, "blocked": 0});
+        }
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {
-                    hostEquals: 'developer.chrome.com'
-                },
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
+
+    loadWebsites();
+})();
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,19 +1,59 @@
-let changeColor = document.getElementById('changeColor');
+(function () {
+    var storage = chrome.storage;
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
+    function updateOnButton() {
+        var onButton = document.getElementById("onButton");
 
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id, {
-                code: 'document.body.style.backgroundColor = "' + color + '";'
-            });
-    });
-};
+        storage.local.get("on", function (item) {
+            if (item.on === true) {
+                onButton.innerText = "Deactivate";
+            } else {
+                onButton.innerText = "Activate";
+            }
+        });
+    }
+
+    function onButtonClick() {
+        storage.local.get(["on", "blocked"], function (item) {
+            console.log(item.on);
+            var on;
+
+            if (item.on === undefined || item.on === false) {
+                on = true;
+            } else {
+                on = false;
+            }
+
+            storage.local.set({"on": on, "blocked": 0});
+
+            updateOnButton();
+            updateIcon();
+        });
+    }
+
+    function optionsButtonClick() {
+        chrome.tabs.create({"url": "settings.html"});
+    }
+
+    function updateAttempts() {
+        var nbAttempts = 0;
+
+        storage.local.get("blocked", function (item) {
+            if (item.blocked !== undefined) {
+                nbAttempts = item.blocked;
+            }
+
+            var number = document.getElementsByTagName("number")[0];
+            number.innerText = nbAttempts;
+        });
+    }
+
+    updateAttempts();
+    updateOnButton();
+    
+    var onButton = document.getElementById("onButton");
+    var optionsButton = document.getElementById("optionsButton");
+
+    onButton.addEventListener("click", onButtonClick);
+    optionsButton.addEventListener("click", optionsButtonClick);
+})();
